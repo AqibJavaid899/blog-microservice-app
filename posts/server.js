8484a646ad24db823673dd5615757467a8621603
javaidaqib <aqibjavaid.ahmed@systemsltd.com>
2023-11-17 23:21:54 +0500

    feature: create the k8s initial setup and also create the posts deployment file with one pod instance

--- posts/server.js
+++ posts/server.js
@@ -1,40 +1,44 @@
-import express from "express"
-import cors from "cors"
-import {randomBytes} from 'crypto'
-import axios from 'axios'
+import express from "express";
+import cors from "cors";
+import { randomBytes } from "crypto";
+import axios from "axios";
 
-const app = express()
+const app = express();
 
 const posts = {};
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/posts", (req, res) => {
-    res.status(200).json(posts)
-})
+  res.status(200).json(posts);
+});
 
 app.post("/posts", async (req, res) => {
-    const id = randomBytes(4).toString("hex")
-    const { title } = req.body;
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    // Add the post in the In-Memory DB
-    posts[id] = { id, title, comments: [] }
+  // Add the post in the In-Memory DB
+  posts[id] = { id, title, comments: [] };
 
-    // Create the event payload and generate the PostCreated event
-    const event = { type: "PostCreated", data: { id, title } }
-    await axios.post("http://localhost:4005/events", event).catch((err) => console.log("Error is : ", err))
+  // Create the event payload and generate the PostCreated event
+  const event = { type: "PostCreated", data: { id, title } };
+  await axios
+    .post("http://localhost:4005/events", event)
+    .catch((err) => console.log("Error is : ", err));
 
-    res.status(201).json(posts[id])
-})
+  res.status(201).json(posts[id]);
+});
 
 app.post("/events", (req, res) => {
-    const { type, data } = req.body;
+  const { type, data } = req.body;
 
-    console.log("Event Recieved is : ", type)
+  console.log("Event Recieved is : ", type);
 
-    res.status(200).json({ status: "OK" })
-})
+  res.status(200).json({ status: "OK" });
+});
 
-
-app.listen(4000, () => console.log("\nPosts listening on Port 4000"));
+app.listen(4000, () => {
+  console.log("There is a new update here... Version 20!!");
+  console.log("\nPosts listening on Port 4000");
+});
